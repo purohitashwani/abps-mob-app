c4828d61d7a2aff88f80bf3100d56e7dc1fce6c0
Ashwani Purohit <ashwani.purohit@dotsquares.com>
2023-05-24 15:23:52 +0530

    design updates

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { Share } from '@capacitor/share';
 
 
 @Component({
@@ -20,6 +21,15 @@ export class SideMenuComponent  implements OnInit {
       setTimeout(()=> {
         this.router.navigate([page]);
       },500)
+  }
+
+  async shareUs() {
+    await Share.share({
+      title: 'श्री गणेश -: पुष्करणा परिवार',
+      text: ' यह एक ऑनलाइन पुष्करणा मंच है। इसे बनाने का उद्देश्य समाज के सभी सम्मानित सदस्यों को एक परिवार के रूप में जोड़ना और सभी के विचारों को आपस में आदान-प्रदान करना है। ताकि समाज को नई दिशा और ऊर्जा मिले।',
+      url: 'https://hi.wikipedia.org/wiki/%E0%A4%AA%E0%A5%81%E0%A4%B7%E0%A5%8D%E0%A4%95%E0%A4%B0%E0%A4%A3%E0%A4%BE',
+      dialogTitle: 'सभी पुष्करणा सदस्यो के साथ साँझा करे।',
+    });
   }
 
 }
